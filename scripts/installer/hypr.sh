eeaaf51c30fd15e775e62754d3cdef507c2e1f49
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-23 23:25:47 +0000

    Update hypr.sh

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -20,6 +20,128 @@ run_command "pacman -S --noconfirm polkit-kde-agent" "Install KDE Polkit agent f
 run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon" "yes"
 run_command "cp -r /home/$SUDO_USER/simple-hyprland/configs/dunst /home/$SUDO_USER/.config/" "Copy dunst config" "yes" "no"
 
-run_command "pacman -S --noconfirm qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
+run_command "yay (bunch of hypr packages)" "Install a full hypr system" "yes"
+yay -S --needed \
+    aquamarine \
+    hyprutils \
+    hyprcursor \
+    hyprwayland-scanner \
+    hyprgraphics \
+    qt5-wayland \
+    qt6-wayland \
+    hyprlang \
+    hyprland-protocols \
+    hyprland-qt-support \
+    hyprland-qtutils \
+    hyprland \
+    hyprlock \
+    hypridle \
+    xdg-desktop-portal \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal-gtk \
+    polkit \
+    hyprpolkitagent \
+    pyprland \
+    dmenu \
+    rofi \
+    waybar \
+    swaync \
+    cmake \
+    wayland-protocols \
+    xorg-xwayland \
+    wlroots \
+    wayland \
+    ranger \
+    hyprpaper \
+    waypaper \
+    swww \
+    mako \
+    ghostty \
+    wdisplays \
+    grim \
+    slurp \
+    pavucontrol \
+    python \
+    pipewire \
+    pipewire-alsa \
+    pipewire-pulse \
+    pipewire-jack \
+    alsa-utils \
+    libinput \
+    libevdev \
+    libxkbcommon \
+    kwayland \
+    wlr-randr \
+    wlr-swaybg \
+    steam-native-runtime \
+    mangohud \
+    sddm \
+    gvfs \
+    thunar \
+    thunar-archive-plugin \
+    stow \
+    iwd \
+    networkmanager \
+    nm-connection-editor \
+    network-manager-applet \
+    zsh \
+    tlp \
+    stremio \
+    fastfetch \
+    cargo \
+    ttf-dejavu \
+    ttf-liberation \
+    ttf-joypixels \
+    ttf-meslo-nerd \
+    tmux \
+    blueman \
+    bluez \
+    bluez-utils \
+    steam \
+    flatpak \
+    discord \
+    wine \
+    winetricks \
+    neovim \
+    lua \
+    libva-nvidia-driver \
+    ripgrep \
+    librewolf-bin \
+    acpi \
+    git \
+    hwinfo \
+    arch-install-scripts \
+    wireless_tools \
+    curl \
+    make \
+    meson \
+    obsidian \
+    man-db \
+    man-pages \
+    xdotool \
+    wget \
+    qutebrowser \
+    zip \
+    unzip \
+    mpv \
+    btop \
+    xarchiver \
+    eza \
+    fzf \
+    mesa \
+    vulkan-mesa-layers \
+    lib32-vulkan-mesa-layers
+
+if lspci | grep -i nvidia > /dev/null; then
+    yay -S --needed \
+        nvidia-dkms \
+        nvidia-utils \
+        nvidia-settings \
+        nvidia-prime \
+        lib32-nvidia-utils \
+        xf86-video-nouveau \
+        opencl-nvidia \
+        lib32-opencl-nvidia \
+fi
 
 echo "------------------------------------------------------------------------"
