b5502e4cc35533cb9dc3a8438021cb6435095782
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-24 03:23:08 +0000

    Update utilities.sh

--- scripts/installer/utilities.sh
+++ scripts/installer/utilities.sh
@@ -23,9 +23,17 @@ run_command "yay -S --sudoloop --noconfirm hyprlock" "Install Hyprlock - Screen 
 
 run_command "yay -S --sudoloop --noconfirm wlogout" "Install Wlogout - Session Manager" "yes" "no"
 
-run_command "yay -S --sudoloop --noconfirm grimblast" "Install Grimblast - Screenshot tool" "yes" "no"
+run_command "yay -S --sudoloop --noconfirm grimblast python" "Install Grimblast - Screenshot tool" "yes" "no"
 
 run_command "yay -S --sudoloop --noconfirm hypridle" "Install Hypridle for idle management (Must)" "yes" "no"
+
+run_command "tempdir=$PWD; git clone https://github.com/hpjansson/chafa.git; cd chafa && ./autogen.sh; make && sudo make install; cd $HOME && python -m venv yt " "Install mov-cli (youtube in the terminal)" "yes" "no"
+
+if [ -d tempdir ]; then
+    cd $HOME && python -m venv yt
+    bash -c "source yt/bin/activate; pip install lxml; pip install mov-cli -U; pip install mov-cli-youtube;"
+    cd $tempdir
+fi
 
 run_command "git clone https://gitlab.torproject.org/tpo/core/arti.git; cd arti; cargo build -p arti --release; sudo mv -f /target/release/arti /usr/bin; cd .. && rm -rf arti" "Install arti - tor in rust" "yes" "no"
 
