a5d7b63d17cad4c3a37c495938c4af8e5c905885
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-24 01:34:37 +0000

    Update install.sh

--- scripts/installer/install.sh
+++ scripts/installer/install.sh
@@ -22,6 +22,7 @@ run_script "prerequisites.sh" "Prerequisites Setup"
 run_script "hypr.sh" "Hyprland & Critical Softwares Setup"
 run_script "utilities.sh" "Basic Utilities & Configs Setup"
 run_script "theming.sh" "Themes and Tools Setup"
+run script "config.sh" "Config Setup"
 run_script "final.sh" "Final Setup"
 
 print_bold_blue "\n🌟 Setup Complete\n"
