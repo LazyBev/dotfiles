5d9f58c007853067e00e5ab66896dd12e833c0e2
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-25 22:51:48 +0000

    Update lspconfig.lua

--- configs/nvim/lua/configs/lspconfig.lua
+++ configs/nvim/lua/configs/lspconfig.lua
@@ -2,19 +2,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "ols" }
 local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
