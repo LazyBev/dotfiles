31300297879ac71a82e69d2fe2e1f6facfd97415
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-26 13:18:50 +0000

    Update prerequisites.sh

--- scripts/installer/prerequisites.sh
+++ scripts/installer/prerequisites.sh
@@ -17,7 +17,8 @@ if run_command "sudo pacman -S --noconfirm --needed git base-devel" "Install YAY
 fi
 run_command "sudo pacman -S --noconfirm pipewire pipewire-alsa pipewire-pulse alsa-utils wireplumber pamixer brightnessctl" "Configuring audio and brightness (Recommended)" "yes" 
 
-run_command "sudo pacman -S --noconfirm yay -S nerd-fonts-git ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono" "Installing Nerd Fonts and Symbols (Recommended)" "yes" 
+# Fonts
+sudo pacman -S --noconfirm yay -S nerd-fonts-git ttf-cascadia-code-nerd ttf-cascadia-mono-nerd ttf-fira-code ttf-fira-mono ttf-fira-sans ttf-firacode-nerd ttf-iosevka-nerd ttf-iosevkaterm-nerd ttf-jetbrains-mono-nerd ttf-jetbrains-mono ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono 
 
 run_command "sudo pacman -S --noconfirm sddm && systemctl enable sddm.service" "Install and enable SDDM (Recommended)" "yes"
 
