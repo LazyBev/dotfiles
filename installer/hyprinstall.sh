8c56609c715e4aa9fdab8533edadc626d91dbc25
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-03-24 09:32:07 +0000

    Update hyprinstall.sh

--- installer/hyprinstall.sh
+++ installer/hyprinstall.sh
@@ -38,9 +38,9 @@ print_info "\nStarting prerequisites setup..."
 sudo pacman -Syu --noconfirm
 
 if ! command -v yay &> /dev/null; then
-    git clone https://aur.archlinux.org/yay-bin.git
-    sudo chown "$USER:$USER" -R yay-bin
-    cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
+    git clone https://aur.archlinux.org/yay.git
+    sudo chown "$USER:$USER" -R yay
+    cd yay && makepkg -si && cd .. && rm -rf yay
 fi
 
 yay -Sy --needed --sudoloop \
