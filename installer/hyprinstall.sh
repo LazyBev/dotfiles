f0667f980df84cdb2843708c85dc116469fecfa4
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-03-24 09:30:43 +0000

    Update hyprinstall.sh

--- installer/hyprinstall.sh
+++ installer/hyprinstall.sh
@@ -312,26 +312,26 @@ sudo systemctl enable sddm.service || echo "Cant enable sddm.service"
 echo -e "\n------------------------------------------------------------------------\n"
 print_info "\nStarting utilities setup..."
 
-# tempdir=$PWD; sudo git clone https://github.com/hpjansson/chafa.git; cd chafa && ./autogen.sh; sudo make && sudo make install; cd $HOME && python -m venv yt 
+tempdir=$PWD; sudo git clone https://github.com/hpjansson/chafa.git; cd chafa && sudo ./autogen.sh; sudo make && sudo make install; cd $HOME && python -m venv yt 
 
-#if [ -d tempdir ]; then
-#    cd $HOME && python -m venv yt
-#    bash -c "source yt/bin/activate; pip install lxml; pip install mov-cli -U; pip install mov-cli-youtube;"
-#    cd $tempdir
-#    cp -r $HOME/simple-hyprland/configs/mov-cli $HOME/.config/
-#fi
+if [ -d tempdir ]; then
+    cd $HOME && python -m venv yt
+    bash -c "source yt/bin/activate; pip install lxml; pip install mov-cli -U; pip install mov-cli-youtube;"
+    cd $tempdir
+    sudo cp -r $HOME/simple-hyprland/configs/mov-cli $HOME/.config/
+fi
 
-# git clone https://gitlab.torproject.org/tpo/core/arti.git; cd arti; cargo build -p arti --release; sudo mv -f /target/release/arti /usr/bin; cd .. && rm -rf arti
+sudo git clone https://gitlab.torproject.org/tpo/core/arti.git; cd arti; cargo build -p arti --release; sudo mv -f /target/release/arti /usr/bin; cd .. && rm -rf arti
 
-# if command -v arti; then
-#    if ! -d $HOME/.config/arti; then
-#        mkdir $HOME/.config/arti
-#    fi
-#    sudo tee $HOME/.config/arti/arti-config.toml <<ART
-#[network]
-#socks_port = 9050
-#ART
-#fi
+if command -v arti; then
+    if ! -d $HOME/.config/arti; then
+        sudo mkdir $HOME/.config/arti
+    fi
+    sudo tee $HOME/.config/arti/arti-config.toml <<ART
+[network]
+socks_port = 9050
+ART
+fi
 
 echo -e "\n------------------------------------------------------------------------\n"
 print_info "\nStarting theming setup..."
