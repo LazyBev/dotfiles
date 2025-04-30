1c20f059a0f772658f944aaa194cd02da93b937d
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-05-01 00:55:58 +0100

    Delete configs/nvim directory

--- configs/nvim/lua/configs/lspconfig.lua
+++
@@ -1,4 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
--- configs/nvim/lua/options.lua
+++
@@ -1,6 +0,0 @@
-require "nvchad.options"
-vim.opt.colorcolumn = "80"
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
