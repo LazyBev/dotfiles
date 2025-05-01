d8e89bbac6c3dc7c9db918bb2322c9ca2fb0eeaf
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-05-01 19:34:40 +0100

    Create nirihyprinstall.sh

---
+++ installer/nirihyprinstall.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+bash -c "~/bev-dotfiles/installer/niriinstall.sh"
+
+yay -S \
+    aquamarine \
+    hyprland \
+    hyprland-protocols \
+    hyprpaper \
+    hyprpicker \
+    hyprshot \
+    hyprcursor \
+    hyprlock \
+    hyprcontrib \
+    hyprsome \
+    hyprland-per-window-layout \
+    hyprlang \
+    hyprpm \
+    waybar \
+    eww \
+    nautilus \
+    wlogout \
+    swayidle \
+    swaylock-effects \
+    grim \
+    slurp \
+    wl-clipboard \
+    wtype \
+    xdg-desktop-portal-hyprland \
+    polkit-kde-agent \
+    swappy \
+    wdisplays \
+    pamixer \
+    pavucontrol \
+    playerctl \
+    brightnessctl \
+    nwg-look \
+    qt5ct \
+    qt6ct \
+    kvantum \
+    btop \
+    waybar-module-pacman-updates \
+    network-manager-applet \
+    wlr-randr \
+    wev \
+    gnome-keyring \
+    xdg-utils \
+    cliphist
+
+  sudo cp -rf "$HOME/bev-dotfiles/config/hypr" "$HOME/.config/"
