c93faa6f1b2449caeeea95505eb46b1b8bd10e29
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-06-05 00:00:55 +0100

    Update withhyprinstall.sh

--- installer/withhyprinstall.sh
+++ installer/withhyprinstall.sh
@@ -19,7 +19,7 @@ trap 'trap_message' INT TERM
 
 echo -e "\n------------------------------------------------------------------------\n"
 
-bash -c "~/bev-dotfiles/installer/install.sh"
+bash -c "~/dotfiles/installer/install.sh"
 
 yay -Syu aquamarine hyprland hyprland-protocols hyprpaper hyprpicker hyprshot hyprcursor hyprlock hyprcontrib hyprsome hyprland-per-window-layout hyprlang hyprpm pyprland waybar eww nautilus wlogout
 yay -Syu swayidle swaylock-effects grim slurp wl-clipboard wtype xdg-desktop-portal-hyprland polkit-kde-agent swappy wdisplays pamixer pavucontrol playerctl brightnessctl nwg-look qt5ct qt6ct kvantum
