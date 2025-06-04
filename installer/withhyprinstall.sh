bab86a96bc4e1bcdffcfce08530b0d1aac3ae80c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-06-05 00:14:07 +0100

    Update withhyprinstall.sh

--- installer/withhyprinstall.sh
+++ installer/withhyprinstall.sh
@@ -25,4 +25,4 @@ yay -Syu aquamarine hyprland hyprland-protocols hyprpaper hyprpicker hyprshot hy
 yay -Syu swayidle swaylock-effects grim slurp wl-clipboard wtype xdg-desktop-portal-hyprland polkit-kde-agent swappy wdisplays pamixer pavucontrol playerctl brightnessctl nwg-look qt5ct qt6ct kvantum
 yay -Syu btop waybar-module-pacman-updates network-manager-applet wlr-randr wev gnome-keyring xdg-utils cliphist
 
-  sudo cp -rf "$HOME/bev-dotfiles/config/hypr" "$HOME/.config/"
+sudo cp -rf "$HOME/bev-dotfiles/config/hypr" "$HOME/.config/"
