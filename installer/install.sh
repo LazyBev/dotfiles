aa385ac49a9a5e3430aa056652013a3ffb485cd2
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2026-01-07 13:22:07 +0000

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -200,6 +200,42 @@ echo -e "\n---------------------------------------------------------------------
 print_info "\nStarting config setup..."
 print_info "\nEverything is recommended to change"
 
+mkdir ~/.config/fontconfig 
+cat > "$HOME/.config/fontconfig/fonts.conf" <<FONTS
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+ <alias>
+   <family>sans-serif</family>
+   <prefer>
+     <family>Noto Sans</family>
+     <family>Noto Color Emoji</family>
+     <family>Noto Emoji</family>
+     <family>DejaVu Sans</family>
+   </prefer> 
+ </alias>
+
+ <alias>
+   <family>serif</family>
+   <prefer>
+     <family>Noto Serif</family>
+     <family>Noto Color Emoji</family>
+     <family>Noto Emoji</family>
+     <family>DejaVu Serif</family>
+   </prefer>
+ </alias>
+
+ <alias>
+  <family>monospace</family>
+  <prefer>
+    <family>Noto Mono</family>
+    <family>Noto Color Emoji</family>
+    <family>Noto Emoji</family>
+   </prefer>
+ </alias>
+</fontconfig>
+FONTS
+
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 ~/.emacs.d/bin/doom sync
