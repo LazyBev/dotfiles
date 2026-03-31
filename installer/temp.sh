592a55cd715d972215d15e22c0f7c801eba2b19d
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2026-03-31 05:00:18 +0100

    Update temp.sh

--- installer/temp.sh
+++ installer/temp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-wipefs -a /dev/nvme0n1 && cd ../..& rm -rf dotfiles && git clone https://github.com/LazyBev/dotfiles && cd dotfiles/installer && chmod +x temp.sh && chmod +x gentoo-install.sh && ./gentoo-install.sh --config gentoo-install.conf
+wipefs -a /dev/nvme0n1 && cd ~ && rm -rf dotfiles && git clone https://github.com/LazyBev/dotfiles && cd dotfiles/installer && chmod +x temp.sh && chmod +x gentoo-install.sh && ./gentoo-install.sh --config gentoo-install.conf
